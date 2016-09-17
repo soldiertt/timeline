77a23b9ab8d5adb36b2a5c2e235f486fd9b99563
soldiertt <soldiertt@gmail.com>
2016-09-17 16:11:32 +0200

    change server port

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    webApiBaseUrl: 'http://localhost:3000/restapi'
+    webApiBaseUrl: 'http://localhost:3200/restapi'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    webApiBaseUrl: 'http://vps313396.ovh.net:3000/restapi'
+    webApiBaseUrl: 'http://vps313396.ovh.net:3200/restapi'
 };
